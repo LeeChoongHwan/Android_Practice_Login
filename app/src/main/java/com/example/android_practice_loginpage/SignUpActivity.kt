93a09cb66fa45c840332929c7965218b80db6832
LeeChoongHwan <choo901@naver.com>
2023-07-31 15:58:46 +0900

    finish view

---
+++ app/src/main/java/com/example/android_practice_loginpage/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_practice_loginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
