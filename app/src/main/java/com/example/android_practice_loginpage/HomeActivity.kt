4198d706295ec16ef9b3d4d52ce28b10a533c030
LeeChoongHwan <choo901@naver.com>
2023-08-01 09:37:15 +0900

    level 3

--- app/src/main/java/com/example/android_practice_loginpage/HomeActivity.kt
+++ app/src/main/java/com/example/android_practice_loginpage/HomeActivity.kt
@@ -2,10 +2,22 @@ package com.example.android_practice_loginpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val textId = findViewById<TextView>(R.id.tv_home_id)
+        val idData = intent.getStringExtra("loginId")
+        val finishButton = findViewById<Button>(R.id.bt_home_exit)
+
+        textId.text = "아이디 : $idData"
+
+        finishButton.setOnClickListener {
+            finish()
+        }
     }
 }
